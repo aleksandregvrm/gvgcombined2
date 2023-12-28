9807618ec47ebe461fd905a933f637227f3bde0e
aleksandregvrm <aleksandregvrm@gmail.com>
2023-12-28 21:20:34 +0400

    authentication added

--- routes/aboutRoutes.js
+++ routes/aboutRoutes.js
@@ -7,8 +7,8 @@ const {
 } = require("../controllers/aboutController");
 const authenticateUser = require("../middleware/authentication");
 
-router.route("/").post(setAboutStats).get(getAboutStats);
-router.route("/:id").patch(editAboutStats);
+router.route("/").post(authenticateUser,setAboutStats).get(getAboutStats);
+router.route("/:id").patch(authenticateUser,editAboutStats);
 
 module.exports = router;
 
