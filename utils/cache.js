659fb56adb58c5e83e4d01f2529a897c451afb8b
aleksandregvrm <aleksandregvrm@gmail.com>
2024-03-22 23:35:57 +0400

    minor changes

--- utils/cache.js
+++ utils/cache.js
@@ -5,7 +5,6 @@ require('dotenv').config();
 const client = redis.createClient({url:process.env.REDIS_URL_EXTERNAL});
 const exec = mongoose.Query.prototype.exec;
 
-
 mongoose.Query.prototype.cache = function ({ query }) {
   this.useCache = true;
   this.allQueries = query;
@@ -16,14 +15,14 @@ mongoose.Query.prototype.exec = async function () {
   if (!this.useCache) {
     return exec.apply(this, arguments);
   }
-  await client.connect();
+  // await client.connect();
   const additionalQueries = this.allQueries;
   const hashName = JSON.stringify(additionalQueries.productsType);
   const key = JSON.stringify(additionalQueries);
   const cachedData = await client.hGet(hashName, key);
   if (cachedData) {
     const doc = JSON.parse(cachedData);
-    client.quit();
+    // client.quit();
     return Array.isArray(doc)
       ? doc.map((d) => new this.model(d))
       : new this.model(doc);
@@ -33,14 +32,14 @@ mongoose.Query.prototype.exec = async function () {
     EX: 300,
     NX: true,
   });
-  client.quit();
+  // client.quit();
   return result;
 };
 
 const cleanCache = async (key) => {
-  await client.connect();
+  // await client.connect();
   client.del(JSON.stringify(key));
-  client.quit();
+  // client.quit();
 };
 
 module.exports = cleanCache;
